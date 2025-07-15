a98b2dfc31e980a9ad4e73e2bf63722fc6e92b4b
Benjamin Vatter <benjaminvatterj@gmail.com>
2025-07-15 13:07:44 -0400

    Add snacks.nvim with git blame and helpers

--- plugins/init.lua
+++ plugins/init.lua
@@ -15,6 +15,15 @@ return {
             return {}
         end,
     },
+    {
+        "folke/snacks.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("snacks").setup(require("configs.snacks"))
+        end,
+    },
+
     -----------------------------------------------------------------
     -- nvim-tree: auto-open when Neovim starts with no file,
     --            auto-quit if it’s the last window
